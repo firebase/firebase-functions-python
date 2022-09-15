06d84b160e3705643da1c752dcea323aa2ce8eda
Salakar <mike.diarmid@gmail.com>
2022-09-15 02:52:22 +0100

    feat: add params module

---
+++ src/firebase_functions/params.py
@@ -0,0 +1,326 @@
+"""Module for params that can make Cloud Functions codebases generic."""
+
+from abc import ABC
+from dataclasses import dataclass
+import os
+from typing import Generic, TypeVar, Optional, Union
+
+T = TypeVar("T", str, int, float, bool, list)
+
+
+@dataclass(frozen=True)
+class Expression(ABC, Generic[T]):
+    """
+    A CEL expression which can be evaluated during function deployment, and
+    resolved to a value of the generic type parameter: i.e, you can pass
+    an Expression<number> as the value of an option that normally accepts numbers.
+    """
+
+    def value(self) -> T:
+        """
+        Returns the Expression's runtime value, based on the CLI's resolution of params.
+        """
+        raise NotImplementedError()
+
+    def to_cel(self) -> str:
+        """
+        Returns the Expression's representation as a braced CEL expression.
+        """
+        return f"{{ {self} }}"
+
+
+def _quote_if_string(literal: T) -> T:
+    return literal if not isinstance(literal, str) else f'"{literal}"'
+
+
+@dataclass(frozen=True)
+class TernaryExpression(Expression[T], Generic[T]):
+    test: Expression[bool]
+    if_true: T
+    if_false: T
+
+    def value(self) -> T:
+        return self.if_true if self.test.value() else self.if_false
+
+    def __str__(self) -> str:
+        return f"{self.test} ? {_quote_if_string(self.if_true)} : {_quote_if_string(self.if_false)}"
+
+
+@dataclass(frozen=True)
+class CompareExpression(Expression[bool], Generic[T]):
+    """
+    A CEL expression that evaluates to boolean true or false based on a comparison
+    between the value of another expression and a literal of that same type.
+    """
+    comparator: str
+    left: Expression[T]
+    right: T
+
+    def value(self) -> bool:
+        left: T = self.left.value()
+        if self.comparator == "==":
+            return left == self.right
+        elif self.comparator == ">":
+            return left > self.right
+        elif self.comparator == ">=":
+            return left >= self.right
+        elif self.comparator == "<":
+            return left < self.right
+        elif self.comparator == "<=":
+            return left <= self.right
+        else:
+            raise ValueError(f"Unknown comparator {self.comparator}")
+
+    def __str__(self) -> str:
+        return f"{self.left} {self.comparator} {_quote_if_string(self.right)}"
+
+    def then(self, if_true: T, if_false: T) -> TernaryExpression[T]:
+        return TernaryExpression(self, if_true, if_false)
+
+
+@dataclass(frozen=True)
+class SelectOptions(Generic[T]):
+    """
+    A representation of an option that can be selected via a SelectInput.
+    """
+
+    value: T
+    """The value of the option."""
+
+    label: Optional[str] = None
+    """The displayed label for the option."""
+
+
+@dataclass(frozen=True)
+class SelectInput(Generic[T]):
+    """
+    Specifies that a Param's value should be determined by having the user select
+    from a list of pre-canned options interactively at deploy-time.
+    """
+
+    options: list[SelectOptions[T]]
+    """A list of user selectable options."""
+
+
+@dataclass(frozen=True)
+class TextInput:
+    """
+    Specifies that a Param's value should be determined by prompting the user
+    to type it in interactively at deploy-time. Input that does not match the provided
+    validation_regex, if present, will be retried.
+    """
+
+    example: Optional[str] = None
+    """
+    An example of the input required that will be displayed alongside the input prompt.
+    """
+
+    validation_regex: Optional[str] = None
+    """
+    Validation regex for the input.
+    Input that does not match this regex, if present, will be retried.
+    """
+
+    validation_error_message: Optional[str] = None
+    """
+    An error message that is displayed to the user if validation_regex fails.
+    """
+
+
+@dataclass(frozen=True)
+class ResourceInput:
+    """
+    Specifies that a Param's value should be determined by having the user
+    select from a list containing all the project's resources of a certain
+    type. Currently, only type:"storage.googleapis.com/Bucket" is supported.
+    """
+
+    type: str
+    """
+    The resouce type. Currently, only type:"storage.googleapis.com/Bucket" is supported.
+    """
+
+
+@dataclass(frozen=True)
+class Param(Expression[T]):
+    """
+    A param is a declared dependency on an external value.
+    """
+
+    name: str
+    """
+    The environment variable of this parameter. Must be upper case.
+    """
+
+    default: Optional[T] = None
+    """
+    The default value to assign to this param if none provided.
+    """
+
+    label: Optional[str] = None
+    """
+    A label that is displayed to the user for this param.
+    """
+
+    description: Optional[str] = None
+    """
+    Description of this param that is displayed to the user.
+    """
+
+    immutable: Optional[bool] = None
+    """
+    Whether the value of this parameter can change between function
+    deployments.
+    """
+
+    input: Union[TextInput, ResourceInput, SelectInput[T]] = TextInput()
+    """
+    The type of input that is required for this param, e.g. TextInput.
+    """
+
+    def value(self) -> T:
+        raise NotImplementedError()
+
+    def compare(self, compare: str, right: T) -> CompareExpression:
+        return CompareExpression(compare, self, right)
+
+    def equals(self, right: T) -> CompareExpression:
+        return self.compare("==", right)
+
+    def __str__(self) -> str:
+        return f"params.{self.name}"
+
+
+@dataclass(frozen=True)
+class SecretParam(Expression[str]):
+    """
+    A secret param is a declared dependency on an external secret.
+    """
+
+    name: str
+    """
+    The environment variable of this parameter. Must be upper case.
+    """
+
+    label: Optional[str] = None
+    """
+    A label that is displayed to the user for this param.
+    """
+
+    description: Optional[str] = None
+    """
+    Description of this param that is displayed to the user.
+    """
+
+    immutable: Optional[bool] = None
+    """
+    Whether the value of this parameter can change between function
+    deployments.
+    """
+
+    def __str__(self):
+        return f"{{{{ params.{self.name} }}}}"
+
+    def value(self) -> str:
+        """Current value of this parameter."""
+        return os.environ.get(self.name, "")
+
+    def compare(self, compare: str, right: T) -> CompareExpression:
+        return CompareExpression(compare, self, right)
+
+    def equals(self, right: T) -> CompareExpression:
+        return self.compare("==", right)
+
+
+@dataclass(frozen=True)
+class StringParam(Param):
+    """A parameter as a string value."""
+
+    def value(self) -> str:
+        if os.environ.get(self.name) is not None:
+            return os.environ[self.name]
+
+        if self.default is not None:
+            return self.default
+
+        return str()
+
+
+@dataclass(frozen=True)
+class IntParam(Param[int]):
+    """A parameter as a int value."""
+
+    def value(self) -> int:
+        if os.environ.get(self.name) is not None:
+            return int(os.environ[self.name])
+        if self.default is not None:
+            return self.default
+        return int()
+
+
+@dataclass(frozen=True)
+class FloatParam(Param[float]):
+    """A parameter as a float value."""
+
+    def value(self) -> float:
+        if os.environ.get(self.name) is not None:
+            return float(os.environ[self.name])
+        if self.default is not None:
+            return self.default
+        return float()
+
+
+@dataclass(frozen=True)
+class BoolParam(Param[bool]):
+    """A parameter as a bool value."""
+
+    def value(self) -> bool:
+        env_value = os.environ.get(self.name)
+        if env_value is not None:
+            if env_value.lower() in ["true", "t", "1", "y", "yes"]:
+                return True
+            if env_value.lower() in ["false", "f", "0", "n", "no"]:
+                return False
+            raise ValueError(f"Invalid value for {self.name}: {env_value}")
+        if self.default is not None:
+            return self.default
+        return False
+
+
+@dataclass(frozen=True)
+class ListParam(Param[list]):
+    """A parameter as a list of strings."""
+
+    def value(self) -> list[str]:
+        if os.environ.get(self.name) is not None:
+            return list(filter(len, os.environ[self.name].split(",")))
+        if self.default is not None:
+            return self.default
+        return []
+
+
+PROJECT_ID = StringParam("GCLOUD_PROJECT",
+                         description="The active Firebase project")
+
+STORAGE_BUCKET = StringParam(
+    "STORAGE_BUCKET",
+    description="The default Cloud Storage for Firebase bucket")
+
+DATABASE_URL = StringParam(
+    "DATABASE_URL",
+    description="The Firebase project's default Realtime Database instance URL",
+)
+
+DATABASE_INSTANCE = StringParam(
+    "DATABASE_INSTANCE",
+    description="The Firebase projet's default Realtime Database instance name",
+)
+
+EXTENSION_ID = StringParam(
+    "EXT_INSTANCE_ID",
+    label="Extension instance ID",
+    description="When a function is running as part of an extension, "
+    "this is the unique identifier for the installed extension "
+    "instance",
+    default="",
+)
---
+++ tests/test_params.py
@@ -0,0 +1,167 @@
+"""BoolParam unit tests."""
+from os import environ
+
+import pytest
+from firebase_functions import params
+
+
+class TestBoolParams:
+    """BoolParam unit tests."""
+
+    def test_bool_param_value_true_or_false(self):
+        """Testing if bool params correctly returns a true or false value."""
+        for value_true, value_false in zip(["true", "t", "1", "y", "yes"],
+                                           ["false", "f", "0", "n", "no"]):
+            environ["bool_value_test"] = value_true
+            assert (params.BoolParam("bool_value_test").value() is
+                    True), "Failure, params returned False"
+            environ["bool_value_test"] = value_false
+            assert (params.BoolParam("bool_value_test").value() is
+                    False), "Failure, params returned True"
+
+    def test_bool_param_value_error(self):
+        """Testing if bool params throws a value error if invalid value."""
+        with pytest.raises(ValueError):
+            environ["bool_value_test"] = "bad_value"
+            params.BoolParam("bool_value_test").value()
+
+    def test_bool_param_empty_default(self):
+        """Testing if bool params defaults to False if no value and no default."""
+        assert (params.BoolParam("bool_default_test").value() is
+                False), "Failure, params returned True"
+
+    def test_bool_param_default(self):
+        """Testing if bool params defaults to provided default value."""
+        assert (params.BoolParam("bool_default_test", default=False).value() is
+                False), "Failure, params returned True"
+        assert (params.BoolParam("bool_default_test", default=True).value() is
+                True), "Failure, params returned False"
+
+    def test_bool_param_equality(self):
+        """Test bool equality."""
+        assert (params.BoolParam("bool_test",
+                                 default=False).equals(False).value() is
+                True), "Failure, equality check returned False"
+        assert (params.BoolParam("bool_test",
+                                 default=True).equals(False).value() is
+                False), "Failure, equality check returned False"
+
+
+class TestFloatParams:
+    """FloatParam unit tests."""
+
+    def test_float_param_value(self):
+        """Testing if float params correctly returns a value."""
+        environ["float_value_test"] = "123.456"
+        assert params.FloatParam("float_value_test",).value() == 123.456, \
+            "Failure, params value != 123.456"
+
+    def test_float_param_empty_default(self):
+        """Testing if float params defaults to empty float if no value and no default."""
+        assert params.FloatParam("float_default_test").value() == float(), \
+            "Failure, params value is not float"
+
+    def test_float_param_default(self):
+        """Testing if float param defaults to provided default value."""
+        assert params.FloatParam("float_default_test", default=float(456.789)).value() == 456.789, \
+            "Failure, params default value != 456.789"
+
+    def test_float_param_equality(self):
+        """Test float equality."""
+        assert (params.FloatParam("float_test",
+                                  default=123.456).equals(123.456).value() is
+                True), "Failure, equality check returned False"
+        assert (params.FloatParam("float_test",
+                                  default=456.789).equals(123.456).value() is
+                False), "Failure, equality check returned False"
+
+
+class TestIntParams:
+    """IntParam unit tests."""
+
+    def test_int_param_value(self):
+        """Testing if int param correctly returns a value."""
+        environ["int_value_test"] = "123"
+        assert params.IntParam(
+            "int_value_test").value() == 123, "Failure, params value != 123"
+
+    def test_int_param_empty_default(self):
+        """Testing if int param defaults to empty int if no value and no default."""
+        assert params.IntParam("int_default_test").value() == int(
+        ), "Failure, params value is not int"
+
+    def test_int_param_default(self):
+        """Testing if int param defaults to provided default value."""
+        assert params.IntParam("int_default_test", default=456).value() == 456, \
+            "Failure, params default value != 456"
+
+    def test_int_param_equality(self):
+        """Test int equality."""
+        assert (params.IntParam("int_test", default=123).equals(123).value() is
+                True), "Failure, equality check returned False"
+        assert (params.IntParam("int_test", default=456).equals(123).value() is
+                False), "Failure, equality check returned False"
+
+
+class TestStringParams:
+    """StringParam unit tests."""
+
+    def test_string_param_value(self):
+        """Testing if string param correctly returns a value."""
+        environ["string_value_test"] = "string_test"
+        assert params.StringParam("string_value_test").value() == "string_test", \
+            'Failure, params value != "string_test"'
+
+    def test_string_param_empty_default(self):
+        """Testing if string param defaults to empty string if no value and no default."""
+        assert params.StringParam("string_default_test").value() == str(), \
+            "Failure, params value is not a string"
+
+    def test_string_param_default(self):
+        """Testing if string param defaults to provided default value."""
+        assert (params.StringParam("string_default_test", default="string_override_default").value()
+                == "string_override_default"), \
+            'Failure, params default value != "string_override_default"'
+
+    def test_string_param_equality(self):
+        """Test string equality."""
+        assert (params.StringParam("string_test",
+                                   default="123").equals("123").value() is
+                True), "Failure, equality check returned False"
+        assert (params.StringParam("string_test",
+                                   default="456").equals("123").value() is
+                False), "Failure, equality check returned False"
+
+
+class TestListParams:
+    """ListParam unit tests."""
+
+    def test_list_param_value(self):
+        """Testing if list param correctly returns list values."""
+        environ["list_value_test"] = "item1,item2"
+        assert params.ListParam("list_value_test").value() == ["item1","item2"], \
+            'Failure, params value != ["item1","item2"]'
+
+    def test_list_param_filter_empty_strings(self):
+        """Testing if list param correctly returns list values wth empty strings excluded."""
+        environ["list_value_test"] = ",,item1,item2,,,item3,"
+        assert params.ListParam("list_value_test").value() == ["item1","item2", "item3"], \
+            'Failure, params value != ["item1","item2", "item3"]'
+
+    def test_list_param_empty_default(self):
+        """Testing if list param defaults to an empty list if no value and no default."""
+        assert params.ListParam("list_default_test").value() == [], \
+            "Failure, params value is not an empty list"
+
+    def test_list_param_default(self):
+        """Testing if list param defaults to the provided default value."""
+        assert (params.ListParam("list_default_test", default=["1", "2"]).value()
+                == ["1", "2"]), \
+            'Failure, params default value != ["1", "2"]'
+
+    def test_list_param_equality(self):
+        """Test list equality."""
+        assert (params.ListParam("list_test", default=["123"]).equals(
+            ["123"]).value() is True), "Failure, equality check returned False"
+        assert (params.ListParam("list_test", default=["456"]).equals(
+            ["123"]).value() is False), "Failure, equality check returned False"
