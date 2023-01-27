94ca397ebb72031c569dc86d4dfc5b25985134a8
pr_Mais <mais.alheraki@gmail.com>
2023-01-27 12:20:57 +0300

    ocs: added README and comments on the tasks sample

--- samples/basic_tasks/functions/main.py
+++ samples/basic_tasks/functions/main.py
@@ -10,7 +10,9 @@ from firebase_functions.options import SupportedRegion, RetryConfig, RateLimits
 
 app = initialize_app()
 
-
+# Once this function is deployed, a Task Queue will be created with the name
+# `on_task_dispatched_example`. You can then enqueue tasks to this queue by
+# calling the `enqueue_task` function.
 @tasks.on_task_dispatched(
     retry_config=RetryConfig(max_attempts=5),
     rate_limits=RateLimits(max_concurrent_dispatches=10),
@@ -23,6 +25,11 @@ def on_task_dispatched_example(req: tasks.CallableRequest):
     print(req.data)
 
 
+# To enqueu a task, you can use the following function.
+# e.g.
+# curl -X POST -H "Content-Type: application/json" \
+#   -d '{"data": "Hello World!"}' \
+#   https://enqueue-task-<projectHash>-<region>.a.run.app\
 @https.on_request()
 def enqueue_task(req: https.Request) -> https.Response:
     """
