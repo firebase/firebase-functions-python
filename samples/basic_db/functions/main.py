d4c9a287f6c36e671a221e954dbd471ccbd2fdb8
Salakar <mike.diarmid@gmail.com>
2023-02-13 21:30:15 +0000

    -

--- samples/basic_db/functions/main.py
+++ samples/basic_db/functions/main.py
@@ -10,20 +10,20 @@ options.set_global_options(region=options.SupportedRegion.EUROPE_WEST1)
 
 
 @db_fn.on_value_written(reference="hello/world")
-def onwriteexample(event: db_fn.Event[db_fn.Change[object]]) -> None:
+def onwriteexample(event: db_fn.Event[db_fn.Change]) -> None:
     print("Hello from db write event:", event)
 
 
 @db_fn.on_value_created(reference="hello/world")
-def oncreatedexample(event: db_fn.Event[object]) -> None:
+def oncreatedexample(event: db_fn.Event) -> None:
     print("Hello from db create event:", event)
 
 
 @db_fn.on_value_deleted(reference="hello/world")
-def ondeletedexample(event: db_fn.Event[object]) -> None:
+def ondeletedexample(event: db_fn.Event) -> None:
     print("Hello from db delete event:", event)
 
 
 @db_fn.on_value_updated(reference="hello/world")
-def onupdatedexample(event: db_fn.Event[db_fn.Change[object]]) -> None:
+def onupdatedexample(event: db_fn.Event[db_fn.Change]) -> None:
     print("Hello from db updated event:", event)
