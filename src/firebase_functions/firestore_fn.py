26f235c614370ca441117e725f46f54e17b0fef4
Salakar <mike.diarmid@gmail.com>
2023-04-06 16:56:24 +0100

    fix: event/change data should be a DocumentSnapshot

--- src/firebase_functions/firestore_fn.py
+++ src/firebase_functions/firestore_fn.py
@@ -20,11 +20,16 @@ import functools as _functools
 import typing as _typing
 import datetime as _dt
 import google.events.cloud.firestore as _firestore
+import google.cloud.firestore_v1 as _firestore_v1
 import firebase_functions.private.util as _util
 import firebase_functions.private.path_pattern as _path_pattern
 import firebase_functions.core as _core
 import cloudevents.http as _ce
 
+from google.cloud._helpers import _datetime_to_pb_timestamp
+from google.cloud.firestore_v1 import _helpers as _firestore_helpers
+
+from google.cloud.firestore_v1 import DocumentSnapshot, DocumentReference
 from firebase_functions.options import FirestoreOptions
 from firebase_functions.core import Change
 
@@ -72,8 +77,8 @@ class Event(_core.CloudEvent[_core.T]):
     """
 
 
-_E1 = Event[Change[_typing.Any | None]]
-_E2 = Event[_typing.Any | None]
+_E1 = Event[Change[DocumentSnapshot | None]]
+_E2 = Event[DocumentSnapshot | None]
 _C1 = _typing.Callable[[_E1], None]
 _C2 = _typing.Callable[[_E2], None]
 
@@ -100,22 +105,53 @@ def _firestore_endpoint_handler(
         raise TypeError(f"Firestore: Cannot parse event payload of data type "
                         f"'{actual_type}' and content type '{content_type}'.")
 
-    if event_type == _event_type_deleted:
-        firestore_event_data = firestore_event_data.value
-    if event_type == _event_type_created:
-        firestore_event_data = firestore_event_data.value
-    if event_type in (_event_type_written, _event_type_updated):
-        before = firestore_event_data.old_value
-        after = firestore_event_data.value
-        firestore_event_data = Change(
-            before=before,
-            after=after,
-        )
     event_location = event_attributes["location"]
     event_project = event_attributes["project"]
     event_namespace = event_attributes["namespace"]
-    event_database = event_attributes["database"]
     event_document = event_attributes["document"]
+    event_database = event_attributes["database"]
+    event_time = _dt.datetime.strptime(
+        event_attributes["time"],
+        "%Y-%m-%dT%H:%M:%S.%f%z",
+    )
+
+    firestore_client = _firestore_v1.Client(database=event_database)
+    firestore_ref: DocumentReference = firestore_client.document(event_document)
+    value_snapshot: DocumentSnapshot | None = None
+    old_value_snapshot: DocumentSnapshot | None = None
+
+    if firestore_event_data.value:
+        document_dict = _firestore_helpers.decode_dict(
+            firestore_event_data.value.fields, firestore_client)
+        value_snapshot = _firestore_v1.DocumentSnapshot(
+            firestore_ref,
+            document_dict,
+            True,
+            _datetime_to_pb_timestamp(event_time),
+            firestore_event_data.value.create_time,
+            firestore_event_data.value.update_time,
+        )
+    if firestore_event_data.old_value:
+        document_dict = _firestore_helpers.decode_dict(
+            firestore_event_data.old_value.fields, firestore_client)
+        old_value_snapshot = _firestore_v1.DocumentSnapshot(
+            firestore_ref,
+            document_dict,
+            True,
+            _datetime_to_pb_timestamp(event_time),
+            firestore_event_data.old_value.create_time,
+            firestore_event_data.old_value.update_time,
+        )
+    if event_type == _event_type_deleted:
+        firestore_event_data = old_value_snapshot
+    if event_type == _event_type_created:
+        firestore_event_data = value_snapshot
+    if event_type in (_event_type_written, _event_type_updated):
+        firestore_event_data = Change(
+            before=old_value_snapshot,
+            after=value_snapshot,
+        )
+
     params: dict[str, str] = {
         **document_pattern.extract_matches(event_document),
     }
@@ -129,10 +165,7 @@ def _firestore_endpoint_handler(
         id=event_attributes["id"],
         source=event_attributes["source"],
         type=event_attributes["type"],
-        time=_dt.datetime.strptime(
-            event_attributes["time"],
-            "%Y-%m-%dT%H:%M:%S.%f%z",
-        ),
+        time=event_time,
         data=firestore_event_data,
         subject=event_attributes["subject"],
         params=params,
@@ -150,7 +183,7 @@ def on_document_written(**kwargs) -> _typing.Callable[[_C1], _C1]:
     .. code-block:: python
 
       @on_document_written(document="*")
-      def example(event: Event[Change[object]]) -> None:
+      def example(event: Event[Change[DocumentSnapshot]]) -> None:
           pass
 
     :param \\*\\*kwargs: Firestore options.
@@ -197,7 +230,7 @@ def on_document_updated(**kwargs) -> _typing.Callable[[_C1], _C1]:
     .. code-block:: python
 
       @on_document_updated(document="*")
-      def example(event: Event[Change[object]]) -> None:
+      def example(event: Event[Change[DocumentSnapshot]]) -> None:
           pass
 
     :param \\*\\*kwargs: Firestore options.
@@ -244,7 +277,7 @@ def on_document_created(**kwargs) -> _typing.Callable[[_C2], _C2]:
     .. code-block:: python
 
         @on_document_created(document="*")
-        def example(event: Event[object]):
+        def example(event: Event[DocumentSnapshot]):
           pass
 
     :param \\*\\*kwargs: Firestore options.
@@ -291,7 +324,7 @@ def on_document_deleted(**kwargs) -> _typing.Callable[[_C2], _C2]:
     .. code-block:: python
 
       @on_document_deleted(document="*")
-      def example(event: Event[object]) -> None:
+      def example(event: Event[DocumentSnapshot]) -> None:
           pass
 
     :param \\*\\*kwargs: Firestore options.
