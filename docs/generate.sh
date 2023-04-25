ff7479a4e69fcb9cf36e306b55ca8da963601d9b
Salakar <mike.diarmid@gmail.com>
2023-04-25 20:50:56 +0100

    docs: add firestore_fn module to docs gen

--- docs/generate.sh
+++ docs/generate.sh
@@ -86,6 +86,7 @@ PY_MODULES='firebase_functions
             firebase_functions.core
             firebase_functions.db_fn
             firebase_functions.eventarc_fn
+            firebase_functions.firestore_fn
             firebase_functions.https_fn
             firebase_functions.identity_fn
             firebase_functions.options
