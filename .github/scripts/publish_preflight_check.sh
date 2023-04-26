5f21c8276970c9e233fc2a628a1b1abd5539eeb5
Daniel Young Lee <taeold@gmail.com>
2023-04-26 15:07:49 -0700

    Remove git fetch instr.

--- .github/scripts/publish_preflight_check.sh
+++ .github/scripts/publish_preflight_check.sh
@@ -136,10 +136,6 @@ echo_info "Checking release tag"
 echo_info "--------------------------------------------"
 echo_info ""
 
-echo_info "---< git fetch --depth=1 origin +refs/tags/*:refs/tags/* >---"
-git fetch --depth=1 origin +refs/tags/*:refs/tags/*
-echo ""
-
 readonly EXISTING_TAG=`git rev-parse -q --verify "refs/tags/v${RELEASE_VERSION}"` || true
 if [[ -n "${EXISTING_TAG}" ]]; then
   echo_warn "Tag v${RELEASE_VERSION} already exists. Exiting."
