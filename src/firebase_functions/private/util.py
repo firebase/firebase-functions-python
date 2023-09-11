e2ea9f62063208d212aa0ddc23c1950d50bca13b
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-10 22:31:05 -0400

    logging.warning doesn't allow null positional (nor keyword) args

--- src/firebase_functions/private/util.py
+++ src/firebase_functions/private/util.py
@@ -143,7 +143,7 @@ def _on_call_valid_content_type(request: _Request) -> bool:
     content_type: str | None = request.headers.get("Content-Type")
 
     if content_type is None:
-        _logging.warning("Request is missing Content-Type.", content_type)
+        _logging.warning("Request is missing Content-Type.")
         return False
 
     # If it has a charset, just ignore it for now.
